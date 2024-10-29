6a1adf9d191b2bd0d3c6a6b24e8146b3c42bb406
hashcowuwu <3189506476@qq.com>
2024-10-29 19:47:53 +0800

    try to fix font but failled

--- app/components/KindTitle.tsx
+++ app/components/KindTitle.tsx
@@ -9,7 +9,7 @@ interface KindTitleProps {
 const KindTitle: React.FC<KindTitleProps> = ({title, link}) =>{
  return (
         <div className="hover:drop-shadow-xl basis-1/2 mx-32 my-4 text-center flex justify-center items-stretch ">
-          <Link className="self-center font-mono" href={link}>{title}</Link>
+          <Link className="self-center font-mono hover:text-glow" href={link}>{title}</Link>
         </div>
  )
 };
