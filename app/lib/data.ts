9247e4fc0638fd13745632697cce0c5631565cba
hashcowuwu <3189506476@qq.com>
2024-11-11 15:44:11 +0800

    asdhuiwe

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,11 +1,1 @@
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432, // 默认 PostgreSQL 端口
-});
-
-export default pool;
+import postg
